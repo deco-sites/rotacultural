6d158e9a1ec60ad8d3f942862e26c987196921fd
Hellen Albuquerque <hellen.albuquerque@auaha.net>
2024-04-11 21:29:47 -0300

    aula 2 e 3

--- loaders/temperature.ts
+++ loaders/temperature.ts
@@ -1,44 +1,44 @@
 interface OpenMeteoWeather {
-    current: {
-        temperature_2m: number;
-    };
-    }
+  current: {
+    temperature_2m: number;
+  };
+}
 
 /**
-   * @title Optionally set lat and long otherwise the user location will be used
-   */
+ * @title Optionally set lat and long otherwise the user location will be used
+ */
 export interface Props {
-    lat?: number;
-    long?: number;
+  lat?: number;
+  long?: number;
 }
 export interface Temperature {
-    celsius: number;
+  celsius: number;
 }
 
 const weatherFor = (lat: number, long: number): string => {
-    return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m`;
+  return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m`;
 };
 
 /**
-    * @title OpenMeteo Temperature
-   */
-    export default async function weather(
-    { lat: _lat, long: _long }: Props,
-    request: Request,
-    ): Promise<Temperature | null> {
-    const lat = _lat ?? request.headers.get("Cf-Iplatitude") ?? undefined;
-    const long = _long ?? request.headers.get("Cf-Iplongitude") ?? undefined;
-    if (!lat || !long) {
-        return null;
-    }
+ * @title OpenMeteo Temperature
+ */
+export default async function weather(
+  { lat: _lat, long: _long }: Props,
+  request: Request,
+): Promise<Temperature | null> {
+  const lat = _lat ?? request.headers.get("Cf-Iplatitude") ?? undefined;
+  const long = _long ?? request.headers.get("Cf-Iplongitude") ?? undefined;
+  if (!lat || !long) {
+    return null;
+  }
 
-    const weatherResponse = await fetch(weatherFor(+lat, +long));
-    if (!weatherResponse.ok) {
-        return null;
-    }
-    const weather: OpenMeteoWeather = await weatherResponse.json();
+  const weatherResponse = await fetch(weatherFor(+lat, +long));
+  if (!weatherResponse.ok) {
+    return null;
+  }
+  const weather: OpenMeteoWeather = await weatherResponse.json();
 
-    return {
-        celsius: weather.current.temperature_2m,
-    };
+  return {
+    celsius: weather.current.temperature_2m,
+  };
 }
--- sections/cupom.tsx
+++ sections/cupom.tsx
@@ -1,14 +1,15 @@
 export interface Props {
-    title: string;
-    description: string;
+  title: string;
+  description: string;
 }
 
 export default function Cupom(props: Props) {
-
-    return (
-        <div className="cupom bg-violet-950 max-w-7xl min-h-80 p-6 m-auto my-6 flex justify-center flex-col rounded-3xl max-sm:m-2.5">
-            <h3 className= "text-indigo-50 text-center font-bold ">{props.title}</h3>
-            <section className= "text-indigo-50 text-center font-normal text-lg">{props.description}</section>
-        </div>
-    );
+  return (
+    <div className="cupom bg-violet-950 max-w-7xl min-h-80 p-6 m-auto my-6 flex justify-center flex-col rounded-3xl max-sm:m-2.5">
+      <h3 className="text-indigo-50 text-center font-bold ">{props.title}</h3>
+      <section className="text-indigo-50 text-center font-normal text-lg">
+        {props.description}
+      </section>
+    </div>
+  );
 }
